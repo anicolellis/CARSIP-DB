98e8e19667846fb5f5a5e09524d2340d70ca6231
Alex Nicolellis <akn5@uchicago.edu>
2026-07-01 16:07:52 -0500

    reformatting and columns

--- carsipdb/__init__.py
+++ carsipdb/__init__.py
@@ -16,10 +16,28 @@ from argparse import ArgumentParser
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-from carsipdb.carsip_models import *
+from carsipdb.carsip_models import (
+    Base,
+    Device,
+    Computer,
+    DHCP,
+    DNS,
+    Sector,
+    Location,
+    Type,
+    Interface,
+    CPU,
+    Memory,
+    MemorySlot,
+    OS,
+    PhysicalDisk,
+    PhysicalDiskType,
+    Volume,
+)
 
 ENGINE = None
 SESSION = None
+
 
 def main() -> None:
     """Main entry point for `carsipdb` console script."""
@@ -35,7 +53,7 @@ def main() -> None:
     parser.add_argument("-n", "--name", type=str, help="Database name.")
     args = parser.parse_args()
 
-    DATABASE_URI=f"postgresql+psycopg2://{args.username}:{args.password}@{args.hostname}:{args.port}/{args.name}"
+    DATABASE_URI = f"postgresql+psycopg2://{args.username}:{args.password}@{args.hostname}:{args.port}/{args.name}"
 
     global ENGINE, SESSION
 
@@ -45,127 +63,107 @@ def main() -> None:
         Base.metadata.drop_all(ENGINE)
     if args.create or args.reset:
         Base.metadata.create_all(ENGINE)
-    
+
     # Test connecting via relationships in both directions for all types
     with Session(ENGINE) as session, session.begin():
-        #Sector
-        sector_13 = Sector(name="13",
-                        modify_time=datetime.datetime.now(),
-                        create_time=datetime.datetime.now())
-        sector_14 = Sector(name="14",
-                        modify_time=datetime.datetime.now(),
-                        create_time=datetime.datetime.now())
-        sector_15 = Sector(name="15",
-                        modify_time=datetime.datetime.now(),
-                        create_time=datetime.datetime.now())
-        #Location
-        lom_13 = Location(name="13 LOM",
-                        modify_time=datetime.datetime.now(),
-                        create_time=datetime.datetime.now())
-        lom_14 = Location(name="14 LOM",
-                        modify_time=datetime.datetime.now(),
-                        create_time=datetime.datetime.now())
-        lom_15 = Location(name="15 LOM",
-                        modify_time=datetime.datetime.now(),
-                        create_time=datetime.datetime.now())           
-        #Type
-        test_type = Type(name="Test Device",
-                        modify_time=datetime.datetime.now(),
-                        create_time=datetime.datetime.now())
-        #Memory
-        test_mem = Memory(capacity=32, speed=5600,
-                            modify_time=datetime.datetime.now(),
-                            create_time=datetime.datetime.now())
-        #CPU
-        test_cpu = CPU(model="Intel Xeon Gold 5415+",
-                        clock_speed=2900,
-                        physical_cores=8,
-                        logical_cores=16,
-                        modify_time=datetime.datetime.now(),
-                        create_time=datetime.datetime.now())
-        #OS
-        win11 = OS(name="Microsoft Windows 11 Enterprise",
-                    version="10.0.26200.8457",
-                    build="25H2",
-                    modify_time=datetime.datetime.now(),
-                    create_time=datetime.datetime.now())
-        #PhysicalDiskType
-        test_disktype = PhysicalDiskType(model="Intel Raid 1 Volume",
-                                        capacity=954,
-                                        type="SSD",
-                                        modify_time=datetime.datetime.now(),
-                                        create_time=datetime.datetime.now())
-        
-        session.add_all([sector_13, sector_14, sector_15, lom_13, lom_14, lom_15, 
-                            test_type, test_mem, test_cpu, win11, test_disktype])
-        #Examples used: CARS5 and CHEMMAT-F126
-        #Device
-        test_device = Device(location_id=1,
-                             sector_id=1,
-                             type_id=1,
-                             description="Test",
-                             name="Test",
-                             make="Test",
-                             model="Test",
-                             serial_number="Test",
-                             uptime=3,
-                             last_online=datetime.datetime.now(),
-                             status="Test",
-                             owners="Alex",
-                             modify_time=datetime.datetime.now(),
-                             create_time=datetime.datetime.now())
-        #Computer
-        test_computer = Computer(device_id=1,
-                                 cpu_id=1,
-                                 bios_version="2.48.0",
-                                 timezone="(UTC-06:00) Central Time (US & Canada)",
-                                 reboot_pending=True,
-                                 cpu_count=1,
-                                 memory_total=64,
-                                 modify_time=datetime.datetime.now(),
-                                 create_time=datetime.datetime.now())
-        #DNS
-        test_dns = DNS(name="Test",
-                       ip_address="1.2.3.4",
-                       modify_time=datetime.datetime.now(),
-                       create_time=datetime.datetime.now())
-        #DHCP
-        test_dhcp = DHCP(device_id=1,
-                         dns_id=1,
-                         name="Test",
-                         mac_address="B0:7B:25:04:A2:B6",
-                         ip_address="164.54.169.126",
-                         modify_time=datetime.datetime.now(),
-                         create_time=datetime.datetime.now())
-        #Interface
-        test_interface = Interface(device_id=1,
-                                   name="Intel Ethernet Connection (5) I219-LM",
-                                   mac_address="B0:7B:25:04:A2:B6",
-                                   manufacturer="Intel",
-                                   speed=1000,
-                                   status="Connected",
-                                   modify_time=datetime.datetime.now(),
-                                   create_time=datetime.datetime.now())
-        #MemorySlot
-        test_mem_slot = MemorySlot(computer_id=1,
-                                   memory_id=1,
-                                   slot_number=0,
-                                   modify_time=datetime.datetime.now(),
-                                   create_time=datetime.datetime.now())
-        #PhysicalDisk
-        test_disk = PhysicalDisk(computer_id=1,
-                                 physical_disk_type_id=1,
-                                 smart_status="OK",
-                                 firmware="1.0.",
-                                 modify_time=datetime.datetime.now(),
-                                 create_time=datetime.datetime.now())
-        #Volume
-        test_volume = Volume(physical_disk_id=1,
-                             capacity=952.6,
-                             file_system="NTFS",
-                             usage="17%",
-                             modify_time=datetime.datetime.now(),
-                             create_time=datetime.datetime.now())
+        # Sector
+        sector_13 = Sector(name="13", modify_time=datetime.datetime.now(), create_time=datetime.datetime.now())
+        sector_14 = Sector(name="14", modify_time=datetime.datetime.now(), create_time=datetime.datetime.now())
+        sector_15 = Sector(name="15", modify_time=datetime.datetime.now(), create_time=datetime.datetime.now())
+        # Location
+        lom_13 = Location(name="13 LOM", modify_time=datetime.datetime.now(), create_time=datetime.datetime.now())
+        lom_14 = Location(name="14 LOM", modify_time=datetime.datetime.now(), create_time=datetime.datetime.now())
+        lom_15 = Location(name="15 LOM", modify_time=datetime.datetime.now(), create_time=datetime.datetime.now())
+        # Type
+        test_type = Type(name="Test Device", modify_time=datetime.datetime.now(), create_time=datetime.datetime.now())
+        # Memory
+        test_mem = Memory(capacity=32, speed=5600, modify_time=datetime.datetime.now(), create_time=datetime.datetime.now())
+        # CPU
+        test_cpu = CPU(
+            model="Intel Xeon Gold 5415+",
+            clock_speed=2900,
+            physical_cores=8,
+            logical_cores=16,
+            modify_time=datetime.datetime.now(),
+            create_time=datetime.datetime.now(),
+        )
+        # OS
+        win11 = OS(
+            name="Microsoft Windows 11 Enterprise",
+            version="10.0.26200.8457",
+            build="25H2",
+            modify_time=datetime.datetime.now(),
+            create_time=datetime.datetime.now(),
+        )
+        # PhysicalDiskType
+        test_disktype = PhysicalDiskType(
+            model="Intel Raid 1 Volume", capacity=954, type="SSD", modify_time=datetime.datetime.now(), create_time=datetime.datetime.now()
+        )
+
+        session.add_all([sector_13, sector_14, sector_15, lom_13, lom_14, lom_15, test_type, test_mem, test_cpu, win11, test_disktype])
+        # Examples used: CARS5 and CHEMMAT-F126
+        # Device
+        test_device = Device(
+            location_id=1,
+            sector_id=1,
+            type_id=1,
+            description="Test",
+            name="Test",
+            make="Test",
+            model="Test",
+            serial_number="Test",
+            uptime=3,
+            last_online=datetime.datetime.now(),
+            status="Test",
+            owners="Alex",
+            modify_time=datetime.datetime.now(),
+            create_time=datetime.datetime.now(),
+        )
+        # Computer
+        test_computer = Computer(
+            device_id=1,
+            cpu_id=1,
+            bios_version="2.48.0",
+            timezone="(UTC-06:00) Central Time (US & Canada)",
+            reboot_pending=True,
+            cpu_count=1,
+            memory_total=64,
+            modify_time=datetime.datetime.now(),
+            create_time=datetime.datetime.now(),
+        )
+        # DNS
+        test_dns = DNS(name="Test", ip_address="1.2.3.4", modify_time=datetime.datetime.now(), create_time=datetime.datetime.now())
+        # DHCP
+        test_dhcp = DHCP(
+            device_id=1,
+            dns_id=1,
+            name="Test",
+            mac_address="B0:7B:25:04:A2:B6",
+            ip_address="164.54.169.126",
+            modify_time=datetime.datetime.now(),
+            create_time=datetime.datetime.now(),
+        )
+        # Interface
+        test_interface = Interface(
+            device_id=1,
+            name="Intel Ethernet Connection (5) I219-LM",
+            mac_address="B0:7B:25:04:A2:B6",
+            manufacturer="Intel",
+            speed=1000,
+            status="Connected",
+            modify_time=datetime.datetime.now(),
+            create_time=datetime.datetime.now(),
+        )
+        # MemorySlot
+        test_mem_slot = MemorySlot(computer_id=1, memory_id=1, slot_number=0, modify_time=datetime.datetime.now(), create_time=datetime.datetime.now())
+        # PhysicalDisk
+        test_disk = PhysicalDisk(
+            computer_id=1, physical_disk_type_id=1, smart_status="OK", firmware="1.0.", modify_time=datetime.datetime.now(), create_time=datetime.datetime.now()
+        )
+        # Volume
+        test_volume = Volume(
+            physical_disk_id=1, capacity=952.6, file_system="NTFS", usage="17%", modify_time=datetime.datetime.now(), create_time=datetime.datetime.now()
+        )
         """Relationships:
         One to one: Computer, Device
                     Device, DHCP
--- carsipdb/carsip_models.py
+++ carsipdb/carsip_models.py
@@ -1,15 +1,20 @@
 import datetime
+from dataclasses import dataclass, field
 from typing import List, Optional
 
 from sqlalchemy import DateTime, Integer, String, Text, Boolean, ForeignKey, Column, Table
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
+
 class Base(DeclarativeBase):
     modify_time: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.now)
     create_time: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.now)
 
+    _columns: dict[str, any] = field(default_factory=dict)
+
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__} - {', '.join(f'{k}: {v}' for k, v in self._columns.items())}>"
+
 
 # Do the association tables need the modify and create times, or is it ok to just have them for the objects they reference?
 computer_os_association_table = Table(
@@ -26,14 +31,16 @@ computer_cpu_association_table = Table(
     Column("cpu_id", ForeignKey("cpu.id"), primary_key=True),
 )
 
+
+@dataclass
 class Device(Base):
     __tablename__ = "device"
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    location_id: Mapped[int] = mapped_column(ForeignKey("location.id")) # Many to one
-    sector_id: Mapped[int] = mapped_column(ForeignKey("sector.id")) # Many to one
-    type_id: Mapped[int] = mapped_column(ForeignKey("type.id")) # Many to one
+    location_id: Mapped[int] = mapped_column(ForeignKey("location.id"))  # Many to one
+    sector_id: Mapped[int] = mapped_column(ForeignKey("sector.id"))  # Many to one
+    type_id: Mapped[int] = mapped_column(ForeignKey("type.id"))  # Many to one
 
     location: Mapped["Location"] = relationship(back_populates="devices")
     sector: Mapped["Sector"] = relationship(back_populates="devices")
@@ -52,13 +59,34 @@ class Device(Base):
     status: Mapped[str] = mapped_column(String(64))
     owners: Mapped[str] = mapped_column(String(64))
 
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "location_id": self.location_id,
+            "sector_id": self.sector_id,
+            "type_id": self.type_id,
+            "description": self.description,
+            "name": self.name,
+            "make": self.make,
+            "model": self.model,
+            "serial_number": self.serial_number,
+            "uptime": self.uptime,
+            "last_online": self.last_online,
+            "status": self.status,
+            "owners": self.owners,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
+
+@dataclass
 class Computer(Base):
     __tablename__ = "computer"
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    device_id: Mapped[int] = mapped_column(ForeignKey("device.id")) # One to one
-    cpu_id: Mapped[int] = mapped_column(ForeignKey("cpu.id")) # Many to many
+    device_id: Mapped[int] = mapped_column(ForeignKey("device.id"))  # One to one
+    cpu_id: Mapped[int] = mapped_column(ForeignKey("cpu.id"))  # Many to many
 
     device: Mapped["Device"] = relationship(back_populates="computer")
     cpus: Mapped[List["CPU"]] = relationship(secondary=computer_cpu_association_table, back_populates="computers")
@@ -72,13 +100,29 @@ class Computer(Base):
     cpu_count: Mapped[int] = mapped_column(Integer)
     memory_total: Mapped[int] = mapped_column(Integer)
 
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "device_id": self.device_id,
+            "cpu_id": self.cpu_id,
+            "bios_version": self.bios_version,
+            "timezone": self.timezone,
+            "reboot_pending": self.reboot_pending,
+            "cpu_count": self.cpu_count,
+            "memory_total": self.memory_total,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
+
+@dataclass
 class DHCP(Base):
     __tablename__ = "dhcp"
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    device_id: Mapped[int] = mapped_column(ForeignKey("device.id")) # One to one
-    dns_id: Mapped[int] = mapped_column(ForeignKey("dns.id")) # One to one
+    device_id: Mapped[int] = mapped_column(ForeignKey("device.id"))  # One to one
+    dns_id: Mapped[int] = mapped_column(ForeignKey("dns.id"))  # One to one
 
     device: Mapped["Device"] = relationship(back_populates="dhcp")
     dns: Mapped["DNS"] = relationship(back_populates="dhcp")
@@ -87,6 +131,20 @@ class DHCP(Base):
     mac_address: Mapped[str] = mapped_column(String(64))
     ip_address: Mapped[str] = mapped_column(String(64))
 
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "device_id": self.device_id,
+            "dns_id": self.dns_id,
+            "name": self.name,
+            "mac_address": self.mac_address,
+            "ip_address": self.ip_address,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
+
+@dataclass
 class DNS(Base):
     __tablename__ = "dns"
 
@@ -97,6 +155,17 @@ class DNS(Base):
     name: Mapped[str] = mapped_column(String(64))
     ip_address: Mapped[str] = mapped_column(String(64))
 
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "name": self.name,
+            "ip_address": self.ip_address,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
+
+@dataclass
 class Type(Base):
     __tablename__ = "type"
 
@@ -106,6 +175,16 @@ class Type(Base):
 
     name: Mapped[str] = mapped_column(String(64))
 
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "name": self.name,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
+
+@dataclass
 class Location(Base):
     __tablename__ = "location"
 
@@ -115,6 +194,16 @@ class Location(Base):
 
     name: Mapped[str] = mapped_column(String(64))
 
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "name": self.name,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
+
+@dataclass
 class Sector(Base):
     __tablename__ = "sector"
 
@@ -124,21 +213,46 @@ class Sector(Base):
 
     name: Mapped[str] = mapped_column(String(64))
 
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "name": self.name,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
+
+@dataclass
 class Interface(Base):
     __tablename__ = "interface"
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    device_id: Mapped[int] = mapped_column(ForeignKey("device.id")) # Many to one
+    device_id: Mapped[int] = mapped_column(ForeignKey("device.id"))  # Many to one
 
     device: Mapped["Device"] = relationship(back_populates="interfaces")
 
     name: Mapped[str] = mapped_column(String(64))
     mac_address: Mapped[str] = mapped_column(String(64))
     manufacturer: Mapped[str] = mapped_column(String(64))
-    speed: Mapped[int] = mapped_column(Integer) #Mbps
+    speed: Mapped[int] = mapped_column(Integer)  # Mbps
     status: Mapped[str] = mapped_column(String(64))
 
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "device_id": self.device_id,
+            "name": self.name,
+            "mac_address": self.mac_address,
+            "manufacturer": self.manufacturer,
+            "speed": self.speed,
+            "status": self.status,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
+
+@dataclass
 class CPU(Base):
     __tablename__ = "cpu"
 
@@ -147,10 +261,23 @@ class CPU(Base):
     computers: Mapped[List["Computer"]] = relationship(secondary=computer_cpu_association_table, back_populates="cpus")
 
     model: Mapped[str] = mapped_column(String(64))
-    clock_speed: Mapped[int] = mapped_column(Integer) #MHz
+    clock_speed: Mapped[int] = mapped_column(Integer)  # MHz
     physical_cores: Mapped[int] = mapped_column(Integer)
     logical_cores: Mapped[int] = mapped_column(Integer)
 
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "model": self.model,
+            "clock_speed": self.clock_speed,
+            "physical_cores": self.physical_cores,
+            "logical_cores": self.logical_cores,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
+
+@dataclass
 class Memory(Base):
     __tablename__ = "memory"
 
@@ -159,21 +286,44 @@ class Memory(Base):
     memory_slots: Mapped[List["MemorySlot"]] = relationship(back_populates="memory")
 
     capacity: Mapped[int] = mapped_column(Integer)
-    speed: Mapped[int] = mapped_column(Integer) #MHz
+    speed: Mapped[int] = mapped_column(Integer)  # MHz
 
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "capacity": self.capacity,
+            "speed": self.speed,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
+
+@dataclass
 class MemorySlot(Base):
     __tablename__ = "memory_slot"
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    computer_id: Mapped[int] = mapped_column(ForeignKey("computer.id")) # Many to one
-    memory_id: Mapped[int] = mapped_column(ForeignKey("memory.id")) # Many to one
+    computer_id: Mapped[int] = mapped_column(ForeignKey("computer.id"))  # Many to one
+    memory_id: Mapped[int] = mapped_column(ForeignKey("memory.id"))  # Many to one
 
     computer: Mapped["Computer"] = relationship(back_populates="memory_slots")
     memory: Mapped["Memory"] = relationship(back_populates="memory_slots")
 
     slot_number: Mapped[int] = mapped_column(Integer)
 
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "computer_id": self.computer_id,
+            "memory_id": self.memory_id,
+            "slot_number": self.slot_number,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
+
+@dataclass
 class OS(Base):
     __tablename__ = "os"
 
@@ -185,13 +335,25 @@ class OS(Base):
     version: Mapped[str] = mapped_column(String(64))
     build: Mapped[str] = mapped_column(String(64))
 
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "name": self.name,
+            "version": self.version,
+            "build": self.build,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
+
+@dataclass
 class PhysicalDisk(Base):
     __tablename__ = "physical_disk"
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    computer_id: Mapped[int] = mapped_column(ForeignKey("computer.id")) # Many to one
-    physical_disk_type_id: Mapped[int] = mapped_column(ForeignKey("physical_disk_type.id")) # Many to one
+    computer_id: Mapped[int] = mapped_column(ForeignKey("computer.id"))  # Many to one
+    physical_disk_type_id: Mapped[int] = mapped_column(ForeignKey("physical_disk_type.id"))  # Many to one
 
     computer: Mapped["Computer"] = relationship(back_populates="physical_disks")
     physical_disk_type: Mapped["PhysicalDiskType"] = relationship(back_populates="physical_disks")
@@ -199,6 +361,18 @@ class PhysicalDisk(Base):
 
     smart_status: Mapped[str] = mapped_column(String(64))
     firmware: Mapped[str] = mapped_column(String(64))
+
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "computer_id": self.computer_id,
+            "physical_disk_type_id": self.physical_disk_type_id,
+            "smart_status": self.smart_status,
+            "firmware": self.firmware,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
 
 class PhysicalDiskType(Base):
     __tablename__ = "physical_disk_type"
@@ -211,15 +385,38 @@ class PhysicalDiskType(Base):
     capacity: Mapped[int] = mapped_column(Integer)
     type: Mapped[str] = mapped_column(String(64))
 
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "model": self.model,
+            "capacity": self.capacity,
+            "type": self.type,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
+
+
+@dataclass
 class Volume(Base):
     __tablename__ = "volume"
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    physical_disk_id: Mapped[int] = mapped_column(ForeignKey("physical_disk.id")) # Many to one
+    physical_disk_id: Mapped[int] = mapped_column(ForeignKey("physical_disk.id"))  # Many to one
 
     physical_disk: Mapped["PhysicalDisk"] = relationship(back_populates="volumes")
 
     capacity: Mapped[int] = mapped_column(Integer)
     file_system: Mapped[str] = mapped_column(String(64))
-    usage: Mapped[str] = mapped_column(String(64)) # How do we want to represent this - percentage or number of GB used
+    usage: Mapped[str] = mapped_column(String(64))  # How do we want to represent this - percentage or number of GB used
+
+    def __post_init__(self) -> None:
+        self._columns = {
+            "id": self.id,
+            "physical_disk_id": self.physical_disk_id,
+            "capacity": self.capacity,
+            "file_system": self.file_system,
+            "usage": self.usage,
+            "modify_time": self.modify_time,
+            "create_time": self.create_time,
+        }
